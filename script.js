1b028159231956220bb97768e187a76c0af35eef
Nikolas Maglica <nikolasmaglica1234@gmail.com>
2025-03-27 21:38:52 +0100

    finalni prijevod

--- script.js
+++ script.js
@@ -8,9 +8,41 @@ const translations = {
         "welcome": "Dobrodošli u Pro<span style='color: #186D88;'>L</span> Systems",
         "expertise": "Stručnjaci smo za razvoj, proizvodnju i montažu razvodnih i upravljačkih ormara te električnih komponenti.",
         "about-button": "O nama",
-        "aboutus":"<strong>O nama</strong>",
-        "aboutus1":"  Bavimo se proizvodnjom, montažom i razvojem elektro komponenti svih vrsta. Bavimo se također ozicavanjem i montažom elektro ormara, ormara za automatizaciju te puštanjem istih u pogon. S obzirom na naša visegodišnja radna iskustva i s ciljem poboljšanja kvalitete našeg rada te kvalitete naših završnih proizvoda, osnovali smo proL systems gmbh.",
-        "aboutus2":" <strong>QUALITY CONNECTION</strong> - kao što naš slogan govori, nudimo sigurna rješenja i garantiramo kvalitetu svih naših proizvoda, kako one za ozicavanje tako i one za montažu i puštanje u rad.   "
+        "aboutus": "<strong>O nama</strong>",
+        "aboutus1": "ProL Systems GmbH je obiteljska tvrtka osnovana 2024. godine. Bavimo se proizvodnjom, montažom i razvojem elektrokontrolnih komponenti svih vrsta. Također se bavimo ožičavanjem i montažom elektroormara, ormara za automatizaciju te puštanjem u pogon istih. S obzirom na naša višegodišnja radna iskustva i s ciljem poboljšanja kvalitete rada te kvalitete završnih proizvoda, došli smo na ideju o osnivanju ProL Systems GmbH.",
+        "aboutus2": "<strong>QUALITY CONNECTION</strong> - Kao što naš slogan govori, nudimo sigurna rješenja i garantiramo kvalitetu svih naših proizvoda, kako ožičavanja tako i montaže i puštanja u rad istih.",
+        "aboutus3": "Poveži se sa svijetom kvalitetnih elektro proizvoda i sigurnih rješenja.",
+        "aboutus4": "Poveži se sa <strong><a href='#contact'>ProL Systems GmbH</a></strong>.",
+        "services_title": "USLUGE",
+        "service1_title": "Montaža",
+        "service1_desc": "Mehanička montaža i ugradnja elektroničkih uređaja i električnih elemenata u skladu s tehničkim nacrtima i specifikacijama.",
+        "service2_title": "Ožičavanje",
+        "service2_desc": "Povezivanje svih elemenata unutar sustava, uključujući kabliranje i spajanje prema nacrtima strujnog kruga.",
+        "service3_title": "Razvoj",
+        "service3_desc": "Dizajniranje i unapređivanje električnih sustava, uključujući izradu i testiranje novih rješenja za optimizaciju performansi.",
+        "service4_title": "Automatizacija",
+        "service4_desc": "Implementacija automatiziranih rješenja u industrijske procese, uključujući programiranje i konfiguraciju elektroničkih elemenata sustava.",
+        "service5_title": "Puštanje u pogon",
+        "service5_desc": "Provjera i analiza električnih instalacija i komponenti u svrhu osiguranja kvalitete, sigurnosti i usklađenosti s tehničkim normama.",
+        "service6_title": "Ispitivanje",
+        "service6_desc": "Završno testiranje i pokretanje električnih sustava nakon instalacije kako bi se osigurala ispravnost i funkcionalnost.",
+        "team_title": "TIM",
+        "founder_director": "Osnivač i direktor",
+        "josip_name": "Josip Lukić",
+        "zvonimir_name": "Zvonimir Lukić",
+        "luka_name": "Luka Lukić",
+        "contact_title": "KONTAKT",
+        "address": "Söllnerstraße 5<br>92637,Weiden in der Oberpfalz",
+        "send_success": "Poruka je poslana. Hvala ti!",
+        "name_placeholder": "Ime i prezime",
+        "name_validation": "Molimo Vas unesite minimalno 4 znaka",
+        "email_placeholder": "Email",
+        "email_validation": "Molimo Vas upišite ispravnu email adresu",
+        "subject_placeholder": "Naslov poruke",
+        "subject_validation": "Molimo unesite najmanje 4 znaka naslova",
+        "message_placeholder": "Poruka",
+        "message_validation": "Napišite poruku",
+        "submit_button": "Pošalji poruku"
     },
     "en": {
         "home": "Home",
@@ -21,9 +53,41 @@ const translations = {
         "welcome": "Welcome to Pro<span style='color: #186D88;'>L</span> Systems",
         "expertise": "We specialize in the development, production, and installation of distribution and control cabinets as well as electrical components.",
         "about-button": "About Us",
-        "aboutus":"<strong>About us</strong>",
-        "aboutus1":"We specialize in the production, assembly, and development of all types of electrical components. Additionally, we handle the maintenance and assembly of electrical cabinets, automation cabinets, and their commissioning. Given our many years of work experience and with the aim of improving the quality of our work and the quality of our final products, we founded proL systems gmbh.",
-        "aboutus2":"<strong>QUALITY CONNECTION</strong> - wie unser Slogan besagt, bieten wir sichere Lösungen und garantieren die Qualität aller unserer Produkte, sowohl für die Verkabelung als auch für die Montage und Inbetriebnahme."
+        "aboutus": "<strong>About Us</strong>",
+        "aboutus1": "ProL Systems GmbH is a family-owned company founded in 2024. We specialize in the production, assembly, and development of all types of electro-control components. We also handle wiring and assembly of electrical cabinets, automation cabinets, and their commissioning. With our many years of work experience and with the aim of improving work quality and the quality of final products, we came up with the idea of establishing ProL Systems GmbH.",
+        "aboutus2": "<strong>QUALITY CONNECTION</strong> - As our slogan states, we offer secure solutions and guarantee the quality of all our products, both in terms of wiring and assembly and commissioning.",
+        "aboutus3": "Connect with the world of quality electrical products and secure solutions.",
+        "aboutus4": "Connect with <strong><a href='#contact'>ProL Systems GmbH</a></strong>.",
+        "services_title": "SERVICES",
+        "service1_title": "Assembly",
+        "service1_desc": "Mechanical assembly and installation of electronic devices and electrical components according to technical drawings and specifications.",
+        "service2_title": "Wiring",
+        "service2_desc": "Connection of all system elements, including cabling and wiring according to circuit diagrams.",
+        "service3_title": "Development",
+        "service3_desc": "Design and improvement of electrical systems, including creation and testing of new solutions for performance optimization.",
+        "service4_title": "Automation",
+        "service4_desc": "Implementation of automated solutions in industrial processes, including programming and configuration of electronic system components.",
+        "service5_title": "Commissioning",
+        "service5_desc": "Inspection and analysis of electrical installations and components to ensure quality, safety and compliance with technical standards.",
+        "service6_title": "Testing",
+        "service6_desc": "Final testing and startup of electrical systems after installation to ensure proper operation and functionality.",
+        "team_title": "TEAM",
+        "founder_director": "Founder & Director",
+        "josip_name": "Josip Lukić",
+        "zvonimir_name": "Zvonimir Lukić",
+        "luka_name": "Luka Lukić",
+        "contact_title": "CONTACT",
+        "address": "Söllnerstraße 5<br>92637,Weiden in der Oberpfalz",
+        "send_success": "Your message has been sent. Thank you!",
+        "name_placeholder": "Your Name",
+        "name_validation": "Please enter at least 4 characters",
+        "email_placeholder": "Your Email",
+        "email_validation": "Please enter a valid email address",
+        "subject_placeholder": "Subject",
+        "subject_validation": "Please enter at least 4 characters of subject",
+        "message_placeholder": "Message",
+        "message_validation": "Please write your message",
+        "submit_button": "Send Message"
 
     },
     "de": {
@@ -35,10 +99,41 @@ const translations = {
         "welcome": "Willkommen bei Pro<span style='color: #186D88;'>L</span> Systems",
         "expertise": "Wir sind Experten für die Entwicklung, Produktion und Montage von Verteilungs- und Steuerungsschränken sowie elektrischen Komponenten.",
         "about-button": "Über uns",
-        "aboutus":"<strong>Über uns</strong>",
-        "aboutus1":"Wir beschäftigen uns mit der Herstellung, Montage und Entwicklung von Elektrokomponenten aller Art. Darüber hinaus kümmern wir uns um die Wartung und Montage von Elektroschränken, Automatisierungsschränken sowie deren Inbetriebnahme. Aufgrund unserer langjährigen Arbeitserfahrung und mit dem Ziel, die Qualität unserer Arbeit und die Qualität unserer Endprodukte zu verbessern, gründeten wir die proL systems gmbh.",
-        "aboutus2":"<strong>QUALITY CONNECTION</strong> - as our slogan says, we offer reliable solutions and guarantee the quality of all our products, both for cabling and for assembly and commissioning."
-
+        "aboutus": "<strong>Über uns</strong>",
+        "aboutus1": "ProL Systems GmbH ist ein familiengeführtes Unternehmen, das im Jahr 2024 gegründet wurde. Wir spezialisieren uns auf die Herstellung, Montage und Entwicklung von elektrischen Steuerungskomponenten aller Art. Darüber hinaus kümmern wir uns um die Verkabelung und Montage von Elektroschränken, Automatisierungsschränken sowie deren Inbetriebnahme. Aufgrund unserer langjährigen Berufserfahrung und mit dem Ziel, die Arbeitsqualität und die Qualität der Endprodukte zu verbessern, kamen wir auf die Idee, die ProL Systems GmbH zu gründen.",
+        "aboutus2": "<strong>QUALITY CONNECTION</strong> - Wie unser Slogan besagt, bieten wir sichere Lösungen und garantieren die Qualität aller unserer Produkte, sowohl bei der Verkabelung als auch bei der Montage und Inbetriebnahme.",
+        "aboutus3": "Verbinden Sie sich mit der Welt qualitativ hochwertiger Elektroprodukte und sicherer Lösungen.",
+        "aboutus4": "Verbinden Sie sich mit <strong><a href='#contact'>ProL Systems GmbH</a></strong>.",
+        "services_title": "DIENSTLEISTUNGEN",
+        "service1_title": "Montage",
+        "service1_desc": "Mechanische Montage und Installation von elektronischen Geräten und elektrischen Komponenten gemäß technischen Zeichnungen und Spezifikationen.",
+        "service2_title": "Verkabelung",
+        "service2_desc": "Verknüpfung aller Systemelemente, einschließlich Verkabelung und Verdrahtung nach Schaltplänen.",
+        "service3_title": "Entwicklung",
+        "service3_desc": "Design und Verbesserung elektrischer Systeme, einschließlich Erstellung und Test neuer Lösungen zur Leistungsoptimierung.",
+        "service4_title": "Automatisierung",
+        "service4_desc": "Implementierung automatisierter Lösungen in industriellen Prozessen, einschließlich Programmierung und Konfiguration elektronischer Systemkomponenten.",
+        "service5_title": "Inbetriebnahme",
+        "service5_desc": "Überprüfung und Analyse elektrischer Anlagen und Komponenten zur Gewährleistung von Qualität, Sicherheit und Einhaltung technischer Normen.",
+        "service6_title": "Prüfung",
+        "service6_desc": "Abschließende Tests und Inbetriebnahme elektrischer Systeme nach der Installation, um ein einwandfreies Funktionieren zu gewährleisten.",
+        "team_title": "TEAM",
+        "founder_director": "Gründer & Geschäftsführer",
+        "josip_name": "Josip Lukić",
+        "zvonimir_name": "Zvonimir Lukić",
+        "luka_name": "Luka Lukić",
+        "contact_title": "KONTAKT",
+        "address": "Söllnerstraße 5<br>92637,Weiden in der Oberpfalz",
+        "send_success": "Ihre Nachricht wurde gesendet. Vielen Dank!",
+        "name_placeholder": "Ihr Name",
+        "name_validation": "Bitte geben Sie mindestens 4 Zeichen ein",
+        "email_placeholder": "Ihre E-Mail",
+        "email_validation": "Bitte geben Sie eine gültige E-Mail-Adresse ein",
+        "subject_placeholder": "Betreff",
+        "subject_validation": "Bitte geben Sie mindestens 4 Zeichen des Betreffs ein",
+        "message_placeholder": "Nachricht",
+        "message_validation": "Bitte schreiben Sie Ihre Nachricht",
+        "submit_button": "Nachricht senden"
     }
 };
 
@@ -48,4 +143,33 @@ function changeLanguage(lang) {
         const key = element.getAttribute('data-key');
         element.innerHTML = translations[lang][key]; // Koristi innerHTML za podršku HTML oznakama
     });
+    updateFormPlaceholders(lang);
+
+}
+function updateFormPlaceholders(lang) {
+    const translations = {
+        "hr": {
+            "name": "Ime i prezime",
+            "email": "Email",
+            "subject": "Naslov poruke",
+            "message": "Poruka"
+        },
+        "en": {
+            "name": "Your Name",
+            "email": "Your Email",
+            "subject": "Subject",
+            "message": "Message"
+        },
+        "de": {
+            "name": "Ihr Name",
+            "email": "Ihre E-Mail",
+            "subject": "Betreff",
+            "message": "Nachricht"
+        }
+    };
+
+    document.getElementById('name').placeholder = translations[lang].name;
+    document.getElementById('email').placeholder = translations[lang].email;
+    document.getElementById('subject').placeholder = translations[lang].subject;
+    document.querySelector('textarea').placeholder = translations[lang].message;
 }
